3db44e512583f8d26096df3f8f664f78ec7e4f33
Akssasori <diniz3003@gmail.com>
2024-08-25 09:25:57 -0300

    adjustment audio

--- src/main/java/com/globo/upcomingAds/controllers/VideoController.java
+++ src/main/java/com/globo/upcomingAds/controllers/VideoController.java
@@ -11,19 +11,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("video")
 public class VideoController {
 
     @GetMapping("/merge-video-audio")
-    public String mergeVideoWithAudio() {
+    public String mergeVideoWithAudio() throws IOException, InterruptedException {
         // Inicializar as bibliotecas do FFmpeg
         avutil.av_log_set_level(avutil.AV_LOG_ERROR);
         swscale.sws_getContext(0, 0, 0, 0, 0, 0, 0, new SwsFilter(), new SwsFilter(),new DoublePointer());
 
         String videoPath = "C:\\hack\\automatizacao\\template.mp4";
         String audioPath = "C:\\hack\\automatizacao\\locucao.wav";
+        String audioTreated = "C:\\hack\\automatizacao\\locucaoOutput.wav";
         String outputPath = "C:\\hack\\automatizacao\\output.mp4";
+
+        removeSilence(audioPath,audioTreated);
 
         try {
             ProcessBuilder pb = new ProcessBuilder(
@@ -43,6 +48,34 @@ public class VideoController {
         } catch (Exception e) {
             e.printStackTrace();
             return "Erro ao criar vídeo: " + e.getMessage();
+        }
+    }
+
+    private String removeSilence(String audioPath, String audioTreated) throws IOException, InterruptedException {
+
+        try {
+            ProcessBuilder pb = new ProcessBuilder(
+                    "ffmpeg",
+                    "-i", audioPath,
+                    "-af", "silenceremove=start_periods=1:start_silence=0.5:start_threshold=-50dB:detection=peak,"
+                    + "reverse,silenceremove=start_periods=1:start_silence=0.5:start_threshold=-50dB:detection=peak,reverse",
+                    audioTreated
+            );
+
+            Process process = pb.start();
+            process.waitFor();
+
+            if (process.exitValue() == 0) {
+                return "Áudio processado com sucesso: " + audioTreated;
+            } else {
+                return "Erro ao processar áudio.";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Erro ao tratar o audio: " + e.getMessage();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "Erro ao tratar o audio: " + e.getMessage();
         }
     }
 
