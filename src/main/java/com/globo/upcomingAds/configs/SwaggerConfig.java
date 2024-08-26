1ae863f7e68533d958bd64e6883ff4cc1fa557b1
Akssasori <diniz3003@gmail.com>
2024-08-25 22:03:22 -0300

    add bean in swagger configuration

--- src/main/java/com/globo/upcomingAds/configs/SwaggerConfig.java
+++ src/main/java/com/globo/upcomingAds/configs/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.globo.upcomingAds.configs;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
 
+    @Bean
     public OpenAPI microserviceOpenAPI(){
         return new OpenAPI().info(
                 new Info().title("upcomingAds")
