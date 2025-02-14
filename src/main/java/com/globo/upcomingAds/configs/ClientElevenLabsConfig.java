0fd1a1ddd4b8f4896db1f0390d0466c50026e550
Lucas <diniz3003@gmail.com>
2025-02-14 10:34:40 -0300

    ajuste feign clients

--- src/main/java/com/globo/upcomingAds/configs/ClientElevenLabsConfig.java
+++ src/main/java/com/globo/upcomingAds/configs/ClientElevenLabsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 
-@Configuration
+//@Configuration
 public class ClientElevenLabsConfig {
 
     @Value("${api.elevenlabs.key}")
